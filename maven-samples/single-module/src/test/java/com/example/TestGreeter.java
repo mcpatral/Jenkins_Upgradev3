4d1b2a504adbc1d7665593e0f16af01227900092
mcpatral <114589753+mcpatral@users.noreply.github.com>
2024-05-25 11:57:24 +0100

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -34,6 +34,13 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  
+  @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "Kelvin";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
